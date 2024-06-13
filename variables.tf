0b23d19829ae433632a47dcba8ff1751e7601ba7
Stephen Smith <stephen.smith.oh@gmail.com>
2024-06-13 04:33:31 -0400

    exploring

--- variables.tf
+++ variables.tf
@@ -1,6 +1,12 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
+variable "region" {
+  type = string
+  description = "aws region"
+  default = "us-east-2"
+}
+
 variable "db_name" {
   description = "Unique name to assign to RDS instance"
 }
